a3add570df2a762000e7ac7b4a77c25a37bc1a3b
zhangjin <228520681@qq.com>
2022-11-12 18:38:01 +0800

    2022/11/12

--- leetcode/C++/Trie.cpp
+++ leetcode/C++/Trie.cpp
@@ -1,21 +1,50 @@
 #include <string>
+#include <vector>
 using namespace std;
 class Trie {
+private:
+    vector<Trie*> children;
+    bool isEnd;
+
+    Trie* searchPrefix(string prefix) {
+        Trie* root=this;
+        for(char ch:prefix)
+        {
+            ch=ch-'a';
+            if(root->children[ch]==NULL)
+            return NULL;
+            root=root->children[ch];
+        }
+        return root;
+    }
 public:
     Trie() {
-
+        vector<Trie*> child(26,NULL);
+        children=child;
+        isEnd=false;
     }
     
     void insert(string word) {
-
+        Trie* root=this;
+        for(char ch:word)
+        {
+            ch-='a';
+            if(root->children[ch]==NULL)
+            {
+                root->children[ch]=new Trie();
+            }
+            root=root->children[ch];
+        }
+        root->isEnd=true;
     }
     
     bool search(string word) {
-
+        Trie* node=searchPrefix(word);
+        return node!=NULL&&node->isEnd;
     }
     
     bool startsWith(string prefix) {
-
+        return this->searchPrefix(prefix)!=NULL;
     }
 };
 
