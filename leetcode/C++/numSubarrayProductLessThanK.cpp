213015d4a83be398792914999c47bbeede79a4cd
zhangjin <228520681@qq.com>
2023-02-10 00:02:35 +0800

    2023/02/09

--- leetcode/C++/numSubarrayProductLessThanK.cpp
+++ leetcode/C++/numSubarrayProductLessThanK.cpp
@@ -3,12 +3,14 @@ using namespace std;
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        int left=0;int right=0;int sum=1;
+        int left=0;int right=0;
+        int sum=1;
         int ans=0;int n=nums.size();
         if(k==0) return 0;
         while(right<n)
         {
             sum*=nums[right];
+            
             while(left<=right&&sum>=k)
             {
                 sum/=nums[left];
